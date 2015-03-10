853e9bc5545e3529126a115570ebd97a56873c63
Jeffrey Wang <jeffwang16@gmail.com>
2015-03-09 22:57:27 -0500

    Update Room.java

--- rooms/Room.java
+++ rooms/Room.java
@@ -6,8 +6,8 @@
 */
 
 public abstract class Room {
-  public String text="You are in a generic room";
-  public boolean visted=false;
+  public String text = "You are in a generic room.";
+  public boolean visted = false;
   
   public void run();
 }
