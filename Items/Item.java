cabb10e1b257f3d43b12c5c30f0be65acb38bb9d
champfish <champfish@users.noreply.github.com>
2015-03-09 22:46:30 -0500

    Update Item.java

--- Items/Item.java
+++ Items/Item.java
@@ -3,6 +3,4 @@ public abstract class Item
   public final String name = "Generic item";
   public final int value = 0;
   public final String description = "It's an item.";
-  
-  public double integrity = 100.0;
 }
