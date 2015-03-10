67ae5909a7c33ceb23ecabff06f4cee11bfe89d9
Jeffrey Wang <jeffwang16@gmail.com>
2015-03-09 22:56:27 -0500

    Update Equipment.java

--- Items/Equipment.java
+++ Items/Equipment.java
@@ -1,7 +1,7 @@
 public abstract class Equipment extends Item
 {
   name = "Equipment";
-  description = "A generic Equiptment";
+  description = "A generic piece of equipment";
   value = 0;
   
   public double integrity = 100.0;
