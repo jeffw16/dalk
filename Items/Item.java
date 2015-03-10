7be7b57cb107f38b8269cad9e5a6c6d65a15dd8a
champfish <champfish@users.noreply.github.com>
2015-03-09 22:39:59 -0500

    Update Item.java

--- Items/Item.java
+++ Items/Item.java
@@ -1,6 +1,6 @@
 public abstract class Item
 {
-  public final String name;
-  public final value;
+  public final String name= "Generic Item";
+  public final value="1337";
   public final description = "A Generic Item";
 }
