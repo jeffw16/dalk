5802683a9e16db6e70703a5d880ea6a798a9a143
champfish <champfish@users.noreply.github.com>
2015-03-11 18:27:11 -0700

    Update Room.java

--- rooms/Room.java
+++ rooms/Room.java
@@ -9,7 +9,7 @@ public abstract class Room {
   public final String unvisitedText = "You are in a generic room.";
   public boolean visited = false;
   public final String visitedText = "You are in a visited generic room";
-  public direction[] directions = new directions[2];
+  public direction[] directions = new direction[2];
   directions = {new direction("Go North to Generic Room", IDKFORIDSYSTEM), new direction("GO SOUTH TO GENERIC ROOM",IDKFORIDSYSTEM)}
   
   public void run()
@@ -23,5 +23,10 @@ public abstract class Room {
     // This code should probably move to a generic call to the engine.
     System.out.println("What do you want to do?");
     System.out.println("1. Open Inventory");
+    
+    for(int i=0;i<directions.length2;i++)
+    {
+      System.out.println((i+2)+". "+directions[i].text);
+    }
   }
 }
