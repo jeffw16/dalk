49164ba21afcd2a595da2d20d074c5a6619fbf8f
Jeffrey Wang <jeffwang16@gmail.com>
2015-03-13 09:35:54 -0600

    Update and rename direction.java to Direction.java

---
+++ rooms/Direction.java
@@ -0,0 +1,11 @@
+// Not sure if this should be in engine or rooms
+
+public class Direction
+{
+  public String text;
+  IDKFORIDSYSTEM
+  public Direction(String text, IDKFORIDSYSTEM) {
+    this.text = text;
+    IDKFORIDSYSTEM
+  }
+}
