c0431468c35cd7b1864538cad924b2bf5f365970
Jeffrey Wang <jeffwang16@gmail.com>
2015-03-13 09:35:01 -0600

    Update Player.java

--- engine/Player.java
+++ engine/Player.java
@@ -3,21 +3,17 @@ import java.util.ArrayList;
 public class Player
 {
   public static String room = "Spawn";
-  public static double maxHealth=100;
-  public static double health= 100; //not sure if should be int or double
+  public static double maxHealth = 100;
+  public static double health = 100; // best to be double for accuracy and future compatibility purposes (see RuneScape healthpoints)
   
   public ArrayList<item.Item> inventory = new ArrayList();
   
-  public void changeHp(double amount)
-  {
-    health+=this.amount;
-    if(heath>maxHealth)
-    {
-      health=maxHealth;
-    }
-    else if(health<=0)
-    {
-      health=0;
+  public void changeHp(double amount) {
+    health += this.amount;
+    if (heath > maxHealth) {
+      health  =maxHealth;
+    } else if ( health <= 0 ) {
+      health = 0;
       System.out.println("\nYou Died!\n");
     }
   }
