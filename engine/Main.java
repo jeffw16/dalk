49c9843e0b981984f63864640bc1864e915e5b79
Jeffrey Wang <jeffwang16@gmail.com>
2015-03-13 19:12:56 -0600

    Update Main.java

--- engine/Main.java
+++ engine/Main.java
@@ -5,10 +5,12 @@
  * @authors CJ Duffee, Jeffrey Wang
 */
 
-public class Main
-{
-  public static void main(String args[])
-  {
-    
+public class Main {
+  public static void main(String args[]) {
+    System.out.println ( "Dalk" );
+    System.out.println ( "Loading..." );
+    // call user
+    // if user exists, run something, else make user profile
+    // start process
   }
 }
