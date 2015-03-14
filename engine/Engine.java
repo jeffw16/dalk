1465496a21bc2370ac7bc0366ff0c5a8cc0db01e
Jeffrey Wang <jeffwang16@gmail.com>
2015-03-13 19:14:25 -0600

    Update Engine.java

--- engine/Engine.java
+++ engine/Engine.java
@@ -1,11 +1,9 @@
-public class Engine
-{
-  public void run()
-  {
-    while(true)
-    {
-      switch(Player.room)
-      {
+public class Engine {
+  public void run() {
+    // this room position really needs to be switched to the Room.java file. Engine.java should
+    // call Room.java's "select room" function
+    while ( true ) {
+      switch ( Player.room ) {
         case "Spawn":
         rooms.Spawn.run();
         break;
