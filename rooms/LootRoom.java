8a2ce506cc7867e695d4e47c22ee9aa0cbd48e82
champfish <champfish@users.noreply.github.com>
2015-03-18 20:29:01 -0500

    Update LootRoom.java

--- rooms/LootRoom.java
+++ rooms/LootRoom.java
@@ -4,4 +4,13 @@ public abstract class LootRoom extends Room
   public ArrayList<Item> loot;
   
   public void addItems();
+  
+  public void run()
+  {
+    super.run();
+    for(int i=super.directions.length+2;i<loot.size();i++)
+    {
+      System.out.println(i+". "+loot.get(i-(super.directions.length+2));
+    }
+  }
 }
