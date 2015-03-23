fc51c703734c934ffa66343061861ac99ea1dab1
champfish <champfish@users.noreply.github.com>
2015-03-22 20:46:51 -0500

    Update Equipment.java

--- items/Equipment.java
+++ items/Equipment.java
@@ -1,7 +1,11 @@
 public abstract class Equipment extends Item
 {
-  name = "Equipment";
+  public Equipment()
+  {
+    name = "Equipment";
   description = "A generic piece of equipment";
+  }
+  
   public int value = 0;
   
   public double integrity = 100.0;
