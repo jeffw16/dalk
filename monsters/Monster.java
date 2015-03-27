f516b132e8c2d20509f5961854550850f02a81a3
Nefaerious <samshin102@gmail.com>
2015-03-27 12:53:44 -0600

    Update Monster.java

--- monsters/Monster.java
+++ monsters/Monster.java
@@ -2,4 +2,7 @@ public abstract class monster
 {
   public int hp=100;
   public String name = "Monster";
+  public int attack = 10;
+  public boolean mood = false; //if boolean mood == true then the monster is angry and will attack
+  
 }
