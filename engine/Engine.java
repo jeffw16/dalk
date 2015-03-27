c2dd6dbff4ed32f8d3ea058c4b4b8c8b8c7a5987
Jeffrey Wang <jeffw16@users.noreply.github.com>
2015-03-27 13:53:46 -0500

    Update Engine.java

--- engine/Engine.java
+++ engine/Engine.java
@@ -1,15 +1,15 @@
 public class Engine {
   public void run() {
-    // call user
-    User user = new User();
+    // call player
+    Player player = new Player();
     // if user exists, run something, else make user profile
     GetInput getinput = new GetInput();
     String username = getinput.requestString ( "Username:" );
-    if ( !user.exists ( username ) ) {
-      user.makeProfile();
+    if ( !player.exists ( username ) ) {
+      player.makeProfile();
     }
     // start process
     // removed function, added to Room.java
-    Room room = new Room ( user.getRoom() );
+    Room room = new Room ( player.getRoom() );
   }
 }
